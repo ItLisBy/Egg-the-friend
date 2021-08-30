004f9e848c392971db504d4d527be0fd7d66487d
LIS <l.ilya@outlook.com>
2021-08-30 16:31:34 +0300

    init

---
+++ src/Tama/ViewModels/ViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Tama.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    { }
+}
