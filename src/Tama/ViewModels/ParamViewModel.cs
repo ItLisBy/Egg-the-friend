5637a853101299373e456e81fdc1f56873b9b6fb
LIS <l.ilya@outlook.com>
2021-08-31 21:44:45 +0300

    diff

--- src/Tama/ViewModels/ParamViewModel.cs
+++ src/Tama/ViewModels/ParamViewModel.cs
@@ -6,9 +6,17 @@ namespace Tama.ViewModels
 {
     public class ParamViewModel : ViewModelBase
     {
-        private Bitmap _image; //Image to be displayed
 
-        public Bitmap Img {
+        private string _type = "base";
+
+        public string Type {
+            get => _type;
+            set => this.RaiseAndSetIfChanged(ref _type, value);
+        }
+
+        private string _image; //Image to be displayed
+
+        public string Img {
             get => _image;
             set => this.RaiseAndSetIfChanged(ref _image, value);
         }
@@ -22,14 +30,14 @@ namespace Tama.ViewModels
                 {
                     this.RaiseAndSetIfChanged(ref _val, value);
                     if (Val == 0) { //Param is 0
-                        Img = new Bitmap("");
+                        Img = "";
                     }
                     else if (Val is <= 25 and > 0) { //Food is from 0 through 25
-                        Img = new Bitmap("");
+                        Img = "";
                     }
                     //...
                     else { //Image if food not in range
-                        Img = new Bitmap("");
+                        Img = "";
                     }
                 }
             }
--- src/Tama/Views/ParamImage.axaml.cs
+++ src/Tama/Views/ParamImage.axaml.cs
@@ -7,16 +7,54 @@ namespace Tama.Views
 {
     public class ParamImage : UserControl
     {
+        public static readonly StyledProperty<string> TypeProperty =
+            AvaloniaProperty.Register<ParamImage, string>(nameof(ParamType));
+
+        public string ParamType {
+            get => GetValue(TypeProperty);
+            set => SetValue(TypeProperty, value);
+        }
 
         public static readonly StyledProperty<int> ValueProperty =
             AvaloniaProperty.Register<ParamImage, int>(nameof(Value));
         
         public int Value {
             get => GetValue(ValueProperty);
-            set => SetValue(ValueProperty, value);
+            set {
+                SetValue(ValueProperty, value);
+                if (value is >= 0 and <= 100) {
+                    if (Value == 0) { //Param is 0
+                        Path = $"/Assets/4_{ParamType}.svg";
+                    }
+                    else if (Value is <= 25 and > 0) { //Food is from 0 through 25
+                        Path = $"/Assets/3_{ParamType}.svg";
+                    }
+                    else if (Value is <= 50 and > 25) { //Food is from 25 through 50
+                        Path = $"/Assets/2_{ParamType}.svg";
+                    }
+                    else if (Value is <= 75 and > 50) { //Food is from 50 through 75
+                        Path = $"/Assets/1_{ParamType}.svg";
+                    }
+                    else if (Value is <= 100 and > 75) { //Food is from 75 through 100
+                        Path = $"/Assets/0_{ParamType}.svg";
+                    }
+                    else { //Image if food not in range
+                        Path = "/Assets/Error.svg";
+                    }
+                }
+            }
+        }
+
+        public static readonly StyledProperty<string> PathProperty =
+            AvaloniaProperty.Register<ParamImage, string>(nameof(Path));
+
+        public string Path {
+            get => GetValue(PathProperty);
+            set => SetValue(PathProperty, value);
         }
 
         public ParamImage() {
+            Path = "Base";
             InitializeComponent();
             
         }
